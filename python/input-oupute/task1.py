c264d96d1c175c2231eb7aa46be218fa3e7442cc
ibodovmuhammad <ibodovmuhammadd@gmail.com>
2024-04-23 18:58:01 +0500

    First commit

---
+++ python/input-oupute/task1.py
@@ -0,0 +1,103 @@
+#a=int(input())
+#b=int(input())
+#while b>a+1:
+#    b-=1
+#    print(b)
+
+#a=int(input("Input a number to check prime or not:"))
+#i=0
+#cnt=0
+#while i<=a:
+#    i+=1
+#    if a%i==0:
+#        cnt+=1
+#  
+#if  cnt>2:
+#    print("The entered number is not prime number.")
+#
+#else:
+#    print("The entered number is a prime number.")
+#a=int(input("Input a number: "))
+#cnt=0
+#while a>=0:
+#    a%=10
+#    cnt=cnt+a
+#    a/=10
+#  
+#print(cnt)
+
+
+#a=int(input())
+#cnt=0
+#i=1
+#while i<=a:
+#    print(i)
+#    print("*")
+#    print(i)
+#    print("=")
+#    print(i*i)
+#    cnt=cnt+i*i
+#    i+=1
+
+#print(cnt)
+
+#a=int(input())
+#i=0
+#j=0
+#while i<=a:
+#    i+=1
+#    while j<=a:
+#        j+=1
+#        if a%j==0:
+#         print(i)
+
+
+# a=int(input("A="))
+# b=int(input("B="))
+# c=int(input("C="))
+# d=int(input("D="))
+
+# while a<=b:
+#     if a%d==c:
+#         print(a)
+#     a+=1
+
+# x=int(input("x="))
+# b=int(input("b="))
+# cnt=0
+# while x<=0:
+#     if x%10==b:
+#         cnt=cnt+1
+#     x/=10
+
+# print(cnt)
+
+# x=int(input("X="))
+# while x>0:
+#     x%=10
+#     print(x)
+#     x/=10
+
+# a=int(input("a="))
+# i=a*2
+# j=0
+# cnt=0
+# while j<i:
+#     j+=1
+#     if j%2!=0:
+#         print(j)
+#         cnt=cnt+j
+
+# print(cnt)
+    
+
+# a=int(input())
+# b=int(input())
+# cnt=0
+# while a<=b:
+#     a+=1
+#     if a%9==0:
+#         print(a)
+#         cnt=cnt+a
+
+# print(cnt)
